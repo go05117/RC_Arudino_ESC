3f335fd54f482aa870b883435cc67710bb1ba3b9
Donghyub Shin <go05117@naver.com>
2021-10-14 20:02:23 +0900

    Update :: 브레이크 번호 8~22 검증 및 테스트 완료

--- control_rc/control_rc.ino
+++ control_rc/control_rc.ino
@@ -4,50 +4,51 @@ int escPin = 5;   // esc 일반 모터 핀 번호
 int servoPin = 9; // 서보 모터 핀 번호
 
 // 스피드 지정 96~177 속도 점점 증가 및 풀스로틀
-int stop1 = 95;
-int stop1 = 178;
+// 정지 8~22
+int stop1 = 8;
+int stop2 = 9;
 int speed1 = 96;
 int speed2 = 97;
 int speed3 = 98;
 int speed4 = 99;
-int count = 1;
+int count = 8;
 int backcount = 1000;
 
 Servo esc;
 Servo servo;
 
-// 정지 :: 속도 95
-void stop1() {
+// 정지 :: 속도 8
+void stop11() {
   esc.write(stop1);
   delay(1);
 }
 
-// 정지 :: 속도 178
-void stop2() {
+// 정지 :: 속도 9
+void stop22() {
   esc.write(stop1);
   delay(1);
 }
 
 // 전진 :: 속도 96
-void speed1() {
+void speed11() {
   esc.write(speed1);
   delay(1);
 }
 
 // 전진 :: 속도 97
-void speed2() {
+void speed22() {
   esc.write(speed2);
   delay(1);
 }
 
 // 전진 :: 속도 98
-void speed3() {
+void speed33() {
   esc.write(speed3);
   delay(1);
 }
 
 // 전진 :: 속도 99
-void speed4() {
+void speed44() {
   esc.write(speed4);
   delay(1);
 }
@@ -69,29 +70,57 @@ void setup() {
 void loop() {
 //  Serial.println((String)"속도 " + speed1);
 ////  servo.write(speed1);
-//  speed1();
+//  speed11();
 //  delay(4000);
 //  Serial.println((String)"속도 " + speed2);
-//  speed2();
+//  speed22();
 //  delay(4000);
 //  Serial.println((String)"속도 " + speed3);
-//  speed3();
+//  speed33();
 //  delay(4000);
-//  Serial.println((String)"정지 95");
-//  stop4();
+//  Serial.println((String)"정지 " + stop1);
+//  stop11();
+//  delay(4000);
+//
+//  Serial.println((String)"속도 " + speed1);
+//  speed11();
+//  delay(4000);
+//  Serial.println((String)"속도 " + speed2);
+//  speed22();
+//  delay(4000);
+//  Serial.println((String)"속도 " + speed3);
+//  speed33();
+//  delay(4000);
+//  Serial.println((String)"정지 " + stop2);
+//  stop22();
 //  delay(4000);
 
-  // 브레이크 테스트
-//  esc.write(96);
-//  delay(4000);
-//  Serial.println((String)"브레이크 번호" + count);
-//  esc.write(count);
-//  delay(2000);
-//  count += 1;
+  // 브레이크 테스트 8-22
+  Serial.println((String)"속도 " + speed1);
+  speed11();
+  delay(1000);
+  Serial.println((String)"속도 " + speed2);
+  speed22();
+  delay(1000);
+  Serial.println((String)"속도 " + speed3);
+  speed33();
+  delay(1000);
+  // 브레이크
+  Serial.println((String)"브레이크 번호 8");
+  esc.write(8);
+  delay(2000);
 
-  // 후진 테스트
-  Serial.println((String)"후진 번호" + backcount);
-  esc.write(178);
-  delay(3000);
-  backcount -= 1;
+  Serial.println((String)"속도 " + speed1);
+  speed11();
+  delay(1000);
+  Serial.println((String)"속도 " + speed2);
+  speed22();
+  delay(1000);
+  Serial.println((String)"속도 " + speed3);
+  speed33();
+  delay(1000);
+  // 브레이크
+  Serial.println((String)"브레이크 번호 77");
+  esc.write(77);
+  delay(2000);
 }
